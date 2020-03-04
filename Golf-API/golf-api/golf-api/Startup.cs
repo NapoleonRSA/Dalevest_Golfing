809aa196d4cd918a0f7b2fddc17e130d99c4174f
Jaap Coetzer <jaap@dankospark.co.za>
2020-03-04 05:34:41 +0200

    password validation off

--- Golf-API/golf-api/golf-api/Startup.cs
+++ Golf-API/golf-api/golf-api/Startup.cs
@@ -74,6 +74,14 @@ namespace golf.Core
             });
 
             #endregion
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 0;
+            });
 
             services.AddCors(options =>
             {
